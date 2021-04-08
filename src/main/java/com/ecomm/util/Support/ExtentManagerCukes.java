069a04334d4b8412fbcaaf39a57e8cf7b5c96426
taritmahajan <mahajan.tarit1@gmail.com>
2021-04-08 13:05:34 +0530

    Adding Cukes Extent

--- src/main/java/com/ecomm/util/Support/ExtentManagerCukes.java
+++ src/main/java/com/ecomm/util/Support/ExtentManagerCukes.java
@@ -6,13 +6,13 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.ExtentSparkReporterConfig;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ExtentManager {
+public class ExtentManagerCukes {
 
     public static ExtentReports report;
     public static ExtentTest test;
     public static ExtentSparkReporter sparkReporter;
 
-    public ExtentManager(String extentReportName){
+    public ExtentManagerCukes(String extentReportName){
         this.report = new ExtentReports();
         this.sparkReporter = new ExtentSparkReporter("test-output/" + extentReportName+".html");
         this.report.attachReporter(sparkReporter);
